bf43a66f266dadaf2bb3c99e9add3baab30a0957
Utkarsh <ignition3951@gmail.com>
2022-02-18 21:05:13 +0530

    Initial commit

---
+++ junit-in-5-minutes/test/com/in5minutes/junit/MyMathTest.java
@@ -0,0 +1,14 @@
+package com.in5minutes.junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MyMathTest {
+
+	@Test
+	void test() {
+		// Absence of failure is success
+	}
+
+}
